437bf29e60820b3c75e911b14498764f7e865fe7
Ravi Teja <raviteja6820@gmail.com>
2020-02-05 07:10:09 +0530

    links edit:

--- src/Components/CreateMemo.js
+++ src/Components/CreateMemo.js
@@ -5,6 +5,11 @@ import DateComponent from './DateComponent';
 import LinksComponent from './LinksComponent';
 import AceEditor from 'react-ace';
 import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-ejs";
+import "ace-builds/src-noconflict/mode-c_cpp";
+import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-twilight";
 import "ace-builds/src-noconflict/theme-eclipse";
 
@@ -13,6 +18,7 @@ function CreateMemo(props) {
     const [rem, setrem] = useState(false)
     const [web, setweb] = useState(false)
     const [links, setlinks] = useState([])
+    const [language, setlanguage] = useState('javascript')
     const keyHandler = (e) => {
         if(e.keyCode===83 && e.ctrlKey) {
             e.preventDefault() ;
@@ -46,7 +52,7 @@ function CreateMemo(props) {
     return ReactDOM.createPortal(
         <div>
             {code ? <AceEditor
-            mode="java"
+            mode={language}
             theme={props.Theme ? "eclipse" : "twilight"}
             focus={true}
             name="UNIQUE_ID_OF_DIV"
@@ -57,28 +63,29 @@ function CreateMemo(props) {
             className="code-editor"
             width="80%"
             height="50%"
-            /> : ''}    
+            /> : ''}
+        {web ? <LinksComponent links={links} setlinks={setlinks}/> : ''}    
+
         <div className={"create-memo " + (props.Theme ? "create-memo-light-bg" : "dark-create-memo-bg text-white")}>
         <input placeholder="Title" name="title" className={props.Theme ? "white create-memo-db input" : "black text-white create-memo-db input"} value={props.value.title} onChange={titleChange}/>
             
-        {web ? <LinksComponent links={links} setlinks={setlinks}/> : '' }
+        
         {code ? <div className="div-create"></div> : <textarea id="text-area" value={props.value.body} onChange={bodyChange} placeholder="Your text goes here" className={props.Theme ? "white" : "black text-white"}/>}
-          
         Code :&nbsp;
         <SliderComp extraOuterClass="small-switch" extraInnerClass="small-bg" id="toggle-code" setfunction={() => {setcode(!code);}}/>&nbsp;
-        {code ? <select className="code-select">
-            <option>Python</option>
-            <option>Java</option>
-            <option>JavaScript</option>
-            <option>C</option>
-            <option>C++</option>
+        {code ? <select className="code-select" value={language} onChange={(e) => setlanguage(e.target.value)}>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+            <option value="javascript">JavaScript</option>
+            <option value="ejs">ejs</option>
+            <option value="c_cpp">C++</option>
+            <option value="html">HTML</option>
         </select> : ''}&nbsp;
         Remainder :&nbsp;
-        
         <SliderComp extraOuterClass="small-switch" extraInnerClass="small-bg" id="toggle-remainder" setfunction={() => {setrem(!rem)}}/>&nbsp;
         {rem ? <DateComponent/> : ''}&nbsp;
         Links :&nbsp;
-        <SliderComp extraOuterClass="small-switch" extraInnerClass="small-bg" id="toggle-web" setfunction={() => {setweb(!web)}}/>&nbsp;&nbsp;
+        <button className="attach-links" id="toggle-web" onClick={() => {setweb(!web);}}><i className="fas fa-link"></i></button>&nbsp;&nbsp;
         <br/>
         <button type="submit" className="save-btn">Save</button>&nbsp;
         <button onClick={closeCreate} className="close-btn">Cancel</button>&nbsp;
--- src/Components/LinksComponent.js
+++ src/Components/LinksComponent.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 function LinksComponent(props) {
     return (
-        <div className="div-create">
-            <div className="div-input">
-            {props.links.map((el, index) => <React.Fragment><input type="text" name={"link" + index} className={"web-links" + (props.Theme ? " weblinks-input" : " darkmode-button text-white")}/><a href={el} target="_blank" rel="noopener noreferrer"><i className="fas fa-paper-plane"></i></a><br/></React.Fragment>)}
+        <div className="create-links">
+            {props.links.map((el, index) => <React.Fragment><input type="text" name={"link" + index} className={"web-links" + (props.Theme ? " weblinks-input" : " darkmode-button text-white")}/>&nbsp;<a href={el} target="_blank" rel="noopener noreferrer"><i className="fas fa-paper-plane"></i></a><br/></React.Fragment>)}
             <button type="submit" onClick={() => {let arr = props.links; arr.push(''); props.setlinks(arr);}} className="links-add-button"><i className={"fas fa-plus" + (props.Theme ? " text-blue" :" text-white")}></i></button>
-        </div>
         </div>
     )
 }
